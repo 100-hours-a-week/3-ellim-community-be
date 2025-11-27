6753bcf9aba51a052da46d17edd89c5dbc977691
gguip1 <gguip7554@naver.com>
2025-11-27 21:59:24 +0900

    fix: HealthControllerTest Helath2

--- src/main/java/gguip1/community/global/health/HealthController.java
+++ src/main/java/gguip1/community/global/health/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/health2")
     public String healthCheck() {
         return "OK";
     }
